e4268b540a6c96aef2c29b45265724dd277e6468
Mael Thomas <mael@qunb.com>
2014-11-12 11:20:26 +0100

    first commit

---
+++ src/doc/tintin.js
@@ -0,0 +1,10 @@
+
+// My bit of doc
+//--------------------
+// Please read this carefully
+function hello() {
+
+  // Do something useless
+  console.log('Bonjour tout le monde')
+
+}
---
+++ src/scripts/components/DoccoLoaderApp.jsx
@@ -0,0 +1,35 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+require('../../../lib/docco/resources/linear/docco.css')
+
+var Doc1 = require('../../../lib/doccoLoader.js!../../doc/tintin.js');
+
+var React = require('react/addons');
+var ReactTransitionGroup = React.addons.TransitionGroup;
+
+// Export React so the devtools can find it
+(window !== window.top ? window.top : window).React = React;
+
+// CSS
+require('../../styles/normalize.css');
+//require('../../styles/main.css');
+
+var imageURL = require('../../images/yeoman.png');
+
+var DoccoLoaderApp = React.createClass({
+  render: function() {
+    return (
+      <div className='main'>
+        <ReactTransitionGroup transitionName="fade">
+          <Doc1/>
+        </ReactTransitionGroup>
+      </div>
+    );
+  }
+});
+React.renderComponent(<DoccoLoaderApp />, document.getElementById('content')); // jshint ignore:line
+
+module.exports = DoccoLoaderApp;
